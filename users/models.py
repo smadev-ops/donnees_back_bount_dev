1556c37bb056361563a13c1f1dbcda98b6e38400
Aidara <sma.aidara@gmail.com>
2025-02-05 09:51:49 +0000

    Initial commit - Ajout des donnees

---
+++ users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Employee(models.Model):
+    full_name=models.CharField(max_length=200)
+    email=models.CharField(max_length=100)
+    contact=models.CharField(max_length=20)
+    address=models.TextField()
+
+    def __str__(self):
+        return self.full_name
