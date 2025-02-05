09c5b084e453edd4f07e5cdb8000dc4c058a6469
Aidara <sma.aidara@gmail.com>
2025-02-05 10:55:09 +0000

    Ajout des donnees en utilisant postgresql pour la gestion de la base de donnees

--- donneetest/settings.py
+++ donneetest/settings.py
@@ -82,12 +82,24 @@ WSGI_APPLICATION = "donneetest.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "bount_donnees",
+        "USER": "postgres",
+        "PASSWORD": "rootsma",
+        "HOST": "localhost",  
+        "PORT": "5432",  
     }
 }
+
 
 
 # Password validation
