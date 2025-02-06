198ae8d62c15542393561e8175afecca978101ca
Aidara <sma.aidara@gmail.com>
2025-02-06 07:57:17 +0000

    deploiement dans render

--- donneetest/settings.py
+++ donneetest/settings.py
@@ -60,7 +60,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = "donneetest.urls"
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+   
+    "https://sen-bout-dev.vercel.app",
     
 ]
 
@@ -86,12 +87,7 @@ WSGI_APPLICATION = "donneetest.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+
 
 DATABASES = {
     "default": {
